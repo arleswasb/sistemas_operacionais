c736daecc70b7b18f6ee83b3d6cbf42306598656
arleswasb <werbert.barradas.390@ufrn.edu.br>
2024-11-05 19:36:47 -0300

    alteração do arquivo read.c

--- PJT/PJT2/readme.c
+++ PJT/PJT2/readme.c
@@ -1,33 +1,31 @@
-## Compila��o e Carregamento
+## Compilacao e Carregamento
 
-1. **Compilar o m�dulo**:
+1. **Compilar o modulo**:
+
     ```bash
     make
     ```
 
-2. **Carregar o m�dulo**:
-    ```bash
-    sudo insmod pid_v1.ko
+2. **Carregar o modulo**:
+
+    ```bash    sudo insmod pid_v1.ko
     ```
 
-3. **Verificar se o m�dulo est� carregado**:
-    ```bash
-    dmesg 
-    ```
-   Se o m�dulo estiver carregado corretamente, voc� ver� /proc/pid Criado como um modulo carregado.
+3. **Verificar se o modulo esta carregado**:
+
+    ```bash    dmesg     ```
+
+   Se o modulo estiver carregado corretamente, voco vera /proc/pid Criado como um modulo carregado.
 
 ## Testes
 
 1. **Escrever um valor no /proc/pid**:
+
     Para escrever um valor de PID, use o comando echo. Substitua "1395" pelo valor desejado:
 
     utilise o comando pgrep "" para verificar um numero de PID ativo no kernel
 
-
-
-    ```bash
-    echo "copie o numero aqui" > /proc/pid
-    ```
+    ```bash    echo "copie o numero aqui" > /proc/pid    ```
 
 2. **Ler o valor formatado no `/proc/pid`**:
 
@@ -36,9 +34,12 @@
     ```bash
     cat /proc/pid
     ```
-    A sa�da dever� ser algo como:
-    ```plaintext
-    command = [bash] pid = [numero] state = [1]
+    A saida devera ser algo como:
+
+    ```
+    command = [bash] 
+    pid = [numero] 
+    state = [1]
     ```
 
 3. **Repetir com outro valor de PID**:
@@ -47,21 +48,22 @@
     echo "outro numero" > /proc/pid
     cat /proc/pid
     ```
-    Sa�da esperada:
+    Saida esperada:
     ---
     command = [bash] pid = [2567] state = [1]
     ```
 
-## Remo��o do M�dulo
+## Remocao do Modulo
 
-Para remover o m�dulo do kernel, use o comando:
-```bash
-sudo rmmod pid2
+Para remover o modulo do kernel, use o comando:
 
-**Verificar se o m�dulo foi descarregado**:
-    ```bash
-    dmesg 
+```bash sudo rmmod pid2
+
+**Verificar se o modulo foi descarregado**:
+
+    ```bash    dmesg 
     ```
-   Se o m�dulo estiver carregado corretamente, voc� ver� /proc/pid Removido como um modulo carregado.
+
+   Se o modulo estiver carregado corretamente, voce vera /proc/pid Removido como um modulo carregado.
 
 
